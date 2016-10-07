b856c9be6fa78fcf545a5458f5aa5d1dbe004ccb
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2016-10-07 11:06:08 -0400

    Update portfile.cmake

--- ports/fcl/portfile.cmake
+++ ports/fcl/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/fcl-master
+    OPTIONS -DCMAKE_TOOLCHAIN_FILE=C:\projects\vcpkg\scripts\buildsystems\vcpkg.cmake
     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
