107b4964b150c5199b6a133b35f6b856b52e155a
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2016-10-07 11:26:32 -0400

    Update portfile.cmake

--- ports/libccd/portfile.cmake
+++ ports/libccd/portfile.cmake
@@ -24,8 +24,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
-file(RENAME ${CURRENT_PACKAGES_DIR}/lib/cmake/ccd ${CURRENT_PACKAGES_DIR}/share/ccd)
+#file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+#file(RENAME ${CURRENT_PACKAGES_DIR}/lib/cmake/ccd ${CURRENT_PACKAGES_DIR}/share/ccd)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
