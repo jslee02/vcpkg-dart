a26ebd5c8a26469b0dda3e7c674483b44bee8937
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2016-10-07 10:33:45 -0400

    Create portfile.cmake

---
+++ ports/fcl/portfile.cmake
@@ -0,0 +1,26 @@
+
+# Common Ambient Variables:
+#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
+#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
+#   PORT is the current port name (zlib, etc)
+#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#
+
+include(${CMAKE_TRIPLET_FILE})
+include(vcpkg_common_functions)
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/flexible-collision-library/fcl/archive/0.5.0.zip"
+    FILENAME "fcl-0.5.0.zip"
+    SHA512 a1611e414c56d70318b8d0b09f7949c3c49a135ea3ffc83369724e94e1cb5ec737e8b18449765f4c8a33084cef87cd2ea2e116b3a17c158c2437dc8e847d5bfb
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/fcl-0.5.0
+    # OPTIONS -DFCL_SOMETHING
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
