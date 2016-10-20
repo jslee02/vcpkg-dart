32f718adb4817125bb718a05ac6422b9808bfc48
Jeongseok Lee <jslee02@users.noreply.github.com>
2016-10-20 10:37:16 -0400

    Update portfile.cmake

--- ports/libccd/portfile.cmake
+++ ports/libccd/portfile.cmake
@@ -9,14 +9,14 @@
 include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/danfis/libccd/archive/v2.0.zip"
-    FILENAME "libccd-2.0.zip"
-    SHA512 3bed2c971d20bea8c656f44383196c9e2dd0fc0cf46aaf0a6bb800b62103d9368a192ec0f92b274fc45733c09bcb3796eed48ce5ba7817e66a566bd0987ad21f
+    URLS "https://github.com/danfis/libccd/archive/master.zip"
+    FILENAME "libccd-master.zip"
+    SHA512 20abff1f08d8cdadb0e8ca3653d42f9aa7b6f54f98c5101a07926289f33399f075a2ffdc39bdb36936219cf7007fddb21bbdf4d15646770707c7d65ac3a12e5e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libccd-2.0
+    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libccd-master
     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
@@ -31,5 +31,5 @@ vcpkg_install_cmake()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(COPY ${CURRENT_BUILDTREES_DIR}/src/libccd-2.0/BSD-LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libccd)
+file(COPY ${CURRENT_BUILDTREES_DIR}/src/libccd-master/BSD-LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libccd)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/libccd/BSD-LICENSE ${CURRENT_PACKAGES_DIR}/share/libccd/copyright)
