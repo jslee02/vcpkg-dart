8d87a46b332c6024164804bf2975905097d5032a
Jeongseok Lee <jslee02@gmail.com>
2018-02-22 08:42:55 -0800

    Update portfile.cmake

--- ports/dartsim/portfile.cmake
+++ ports/dartsim/portfile.cmake
@@ -1,26 +1,34 @@
 # Common Ambient Variables:
-#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
-#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
-#   PORT is the current port name (zlib, etc)
-#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
 #
 
-include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/v6.3.0)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/dartsim/dart/archive/v6.3.0.zip"
-    FILENAME "dart-6.3.0.zip"
-    SHA512 be7992a3bab53966bb08449271fce002ed2a845044b7480725a847e0bcf1e63087992fda8ff820f32cf4fb2290323dc17b8da75f6684184bc3b2a1c57a7303a4
+    URLS "https://github.com/dartsim/dart/archive/v6.3.0.tar.gz"
+    FILENAME "v6.3.0.tar.gz"
+    SHA512 af0d8c43fb9479d11231b0319e3d4e23d78b2123edf4c76e69f8f0cf7d6770c1af538847e0fbb89bc18ac6028f6475066574a0c3a8962387cdc483141b07e118
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dart-6.3.0
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1
+    SOURCE_PATH ${SOURCE_PATH}
+    # PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
     OPTIONS -DDART_VERBOSE=ON
 )
 
-vcpkg_install_cmake()  
+vcpkg_install_cmake()
+
+# Handle copyright
+# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/dartsim RENAME copyright)
