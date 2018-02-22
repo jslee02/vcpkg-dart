45ea9f986c69d97c00c32c9f9cc04b1e2d592b36
Jeongseok Lee <jslee02@gmail.com>
2018-02-22 08:35:13 -0800

    Rename to dartsim

--- ports/dartsim/portfile.cmake
+++ ports/dartsim/portfile.cmake
@@ -9,14 +9,14 @@
 include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/dartsim/dart/archive/v6.1.1.zip"
-    FILENAME "dart-6.1.1.zip"
+    URLS "https://github.com/dartsim/dart/archive/v6.3.0.zip"
+    FILENAME "dart-6.3.0.zip"
     SHA512 be7992a3bab53966bb08449271fce002ed2a845044b7480725a847e0bcf1e63087992fda8ff820f32cf4fb2290323dc17b8da75f6684184bc3b2a1c57a7303a4
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dart-6.1.1
+    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dart-6.3.0
     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
